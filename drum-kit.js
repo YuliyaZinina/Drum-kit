babc008a62646a36f6b41eb494aca9a287838d83
Yuliya Lobanova <yuliyazinina@gmail.com>
2017-01-20 21:34:31 +0800

    Код

---
+++ drum-kit.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', (e) => {
+	const audio = document.querySelector(`audio[data-key='${e.keyCode}']`),
+		key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+	if (!audio)
+		return;
+	key.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
+
+}, false);
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform')
+		return;
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
